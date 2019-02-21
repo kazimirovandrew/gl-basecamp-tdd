cec416485081ea05a51f5c1a220786bb5866292c
Andrew <kazimirovandrew@gmail.com>
2019-02-21 13:44:32 +0200

    add implementation after replacement

--- src/main/java/Battle.java
+++ src/main/java/Battle.java
@@ -2,13 +2,24 @@ public class Battle {
 
     public String fight(String stringOfMoves) {
 
-        if(stringOfMoves == null){
-            throw new NullPointerException("String of moves is null");
+        String fightOutcome = null;
+
+        try{
+            if(stringOfMoves == null){
+                throw new NullPointerException("String of moves is null");
+            }
+            else if(stringOfMoves.indexOf('!') == -1 && stringOfMoves.indexOf('?') == -1 && stringOfMoves.indexOf('*') == -1){
+                throw new IllegalArgumentException("String of moves = '" + stringOfMoves + "'");
+            }
+            else{
+                fightOutcome = "You died :(";
+            }
         }
-        else if(stringOfMoves.indexOf('!') == -1 && stringOfMoves.indexOf('?') == -1 && stringOfMoves.indexOf('*') == -1){
-            throw new IllegalArgumentException("String of moves = '" + stringOfMoves + "'");
+        catch (RuntimeException ex){
+            fightOutcome = "You won!";
+            ex.printStackTrace();
         }
 
-        return "You won!";
+        return fightOutcome;
     }
 }
