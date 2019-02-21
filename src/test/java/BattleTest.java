e6b4d4765def9b168e4d2d8733857bb782fcd4e8
Andrew <kazimirovandrew@gmail.com>
2019-02-21 10:41:31 +0200

    add 2 test

--- src/test/java/BattleTest.java
+++ src/test/java/BattleTest.java
@@ -1,12 +1,24 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class BattleTest {
+    private static Battle unit;
+
+    @BeforeClass
+    public static void init() {
+        unit = new Battle();
+    }
 
     @Test
     public void givenStringWithMovesThenReturnOutcomeOfBattle(){
-        Battle unit = new Battle();
         assertTrue("You won!".equals(unit.fight()) || "You died :(".equals(unit.fight()));
+    }
+
+    @Test
+    public void givenEmptyStringFromConsoleThenReturnDefaultStringOfMoves(){
+        assertEquals("!default!move!", unit.readFromConsole());
     }
 }
