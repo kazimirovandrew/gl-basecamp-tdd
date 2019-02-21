353e488990b9daee6662b9b53a6da17e90ee3110
Andrew <kazimirovandrew@gmail.com>
2019-02-21 10:20:35 +0200

    add first(functional) test

--- src/test/java/BattleTest.java
+++ src/test/java/BattleTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class BattleTest {
 
+    @Test
+    public void givenStringWithMovesThenReturnOutcomeOfBattle(){
+        Battle unit = new Battle();
+        assertTrue("You won!".equals(unit.fight()) || "You died :(".equals(unit.fight()));
+    }
 }
