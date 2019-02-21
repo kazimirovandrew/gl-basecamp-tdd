b3a9c3c2ec3acb4c787e32a5bfc705451da700db
Andrew <kazimirovandrew@gmail.com>
2019-02-21 12:53:24 +0200

    add implementation after 3,4 tests

--- src/main/java/Battle.java
+++ src/main/java/Battle.java
@@ -2,7 +2,10 @@ public class Battle {
 
     public String fight(String stringOfMoves) {
 
-        if(stringOfMoves.isEmpty()){
+        if(stringOfMoves == null){
+            throw new NullPointerException("String of moves is null");
+        }
+        else if(stringOfMoves.indexOf('!') == -1 && stringOfMoves.indexOf('?') == -1 && stringOfMoves.indexOf('*') == -1){
             throw new IllegalArgumentException("String of moves = '" + stringOfMoves + "'");
         }
 
